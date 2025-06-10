88f112acebf65c843db58eb14f50b22d2d298308
Ben Cuff <bcuff@meta.com>
2025-06-10 12:01:33 -0700

    Add search bar component with styling

---
+++ front-end-kudos-board/src/components/board/search-bar.jsx
@@ -0,0 +1,25 @@
+import useTheme from "../../hooks/useTheme";
+import "./search-bar.css";
+
+export default function SearchBar() {
+	const { colors } = useTheme();
+	return (
+		<form className="form-container">
+			<input placeholder="Search boards..." className="search-input" />
+			<button
+				className="search-btn"
+				type="submit"
+				style={{ background: colors.button, color: colors.background }}
+			>
+				Search
+			</button>
+			<button
+				type="button"
+				className="search-btn"
+				style={{ background: colors.button, color: colors.background }}
+			>
+				Clear
+			</button>
+		</form>
+	);
+}
