3a3e1299e7aca311148a7fde01c4bdc09bb15d8b
Ben Cuff <bcuff@meta.com>
2025-06-10 14:47:17 -0700

    add CreateModal theme support and close button functionality

--- front-end-kudos-board/src/components/board/create-modal.jsx
+++ front-end-kudos-board/src/components/board/create-modal.jsx
@@ -1,7 +1,10 @@
 import propTypes from "prop-types";
+import { useTheme } from "../../hooks/use-theme";
 import "./create-modal.css";
 
 export default function CreateModal({ setToggleCreateModal }) {
+	const { colors } = useTheme();
+
 	const handleSubmit = async (e) => {
 		e.preventDefault();
 		const title = e.target.title.value;
@@ -24,7 +27,7 @@ export default function CreateModal({ setToggleCreateModal }) {
 
 	return (
 		<div className="overlay-style">
-			<div className="modal-style">
+			<div className="modal-style" style={{background: colors.background}}>
 				<h2>Create New Board</h2>
 				<form onSubmit={handleSubmit}>
 					<input
@@ -38,7 +41,7 @@ export default function CreateModal({ setToggleCreateModal }) {
 						type="text"
 						id="board-author"
 						name="author"
-						placeholder="author"
+						placeholder="author (optional)"
 					/>
 					<input
 						type="text"
@@ -54,6 +57,14 @@ export default function CreateModal({ setToggleCreateModal }) {
 					</select>
 					<button type="submit">Create Board</button>
 				</form>
+				<button
+					className="close-btn"
+					onClick={() => {
+						setToggleCreateModal(false);
+					}}
+				>
+					Close
+				</button>
 			</div>
 		</div>
 	);
