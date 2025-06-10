d8abc1fb7dc0fb937126c81a3ea48073442ac55d
Ben Cuff <bcuff@meta.com>
2025-06-10 16:40:25 -0700

    Add react-router for routing and refactor main.jsx to include routes

--- front-end-kudos-board/src/main.jsx
+++ front-end-kudos-board/src/main.jsx
@@ -1,13 +1,22 @@
 import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router";
 import App from "./App.jsx";
+import Card from "./card.jsx";
 import { ThemeProvider } from "./context/theme-content.jsx";
 import "./index.css";
 
-createRoot(document.getElementById("root")).render(
+const root = document.getElementById("root");
+
+ReactDOM.createRoot(root).render(
 	<StrictMode>
 		<ThemeProvider>
-			<App />
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<App />} />
+					<Route path="/:cardId" element={<Card />} />
+				</Routes>
+			</BrowserRouter>
 		</ThemeProvider>
 	</StrictMode>
 );
