e1dffc82b8e178e993e7b32735e1cc2a0b087c44
Ben Cuff <bencuff14@gmail.com>
2025-06-11 07:19:17 -0700

    remove extra reused code from card class

--- front-end-kudos-board/src/components/card/card.jsx
+++ front-end-kudos-board/src/components/card/card.jsx
@@ -1,16 +1,14 @@
 import propTypes from "prop-types";
-import { useNavigate } from "react-router";
 
 export default function Card({ board, card, handleDeleteCard }) {
-	const navigate = useNavigate();
 	return (
 		<div className="card">
 			<img src={card.image} alt={card.title} className="card-image" />
 			<h2 className="card-title">{card.title}</h2>
 			<h3 className="card-category">{card.category}</h3>
 			<div className="btns-container">
-				<button onClick={() => navigate(`/${card.id}`)}>
-					View Card
+                <button onClick={ }>
+					View Comments
 				</button>
 				<button onClick={() => handleDeleteCard(card.id)}>
 					Delete Card
