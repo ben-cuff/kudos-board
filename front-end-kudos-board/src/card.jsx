a53e73ca86501d77a8879d41ee8302a15d71dfc6
Ben Cuff <bcuff@meta.com>
2025-06-10 17:03:18 -0700

    add fetching of dynamic data add some basic styles

--- front-end-kudos-board/src/card.jsx
+++ front-end-kudos-board/src/card.jsx
@@ -1,27 +1,56 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router";
+import "./card.css";
+import ChangeTheme from "./components/change-theme";
+import { useTheme } from "./hooks/use-theme";
 
 export default function Card() {
 	const [boardData, setBoardData] = useState({});
 	const [cardData, setCardData] = useState([]);
-	let { cardId } = useParams();
+	const { boardId } = useParams();
+	const { colors } = useTheme();
+
+	document.body.style.backgroundColor = colors.background;
 
 	useEffect(() => {
-		async () => {
+		(async () => {
 			const responseBoard = await fetch(
-				`${import.meta.env.VITE_BASE_URL}/board`
+				`${import.meta.env.VITE_BASE_URL}/board/${boardId}`
 			);
 			const dataBoard = await responseBoard.json();
-			console.log(dataBoard);
+
 			setBoardData(dataBoard);
 
 			const responseCard = await fetch(
-				`${import.meta.env.VITE_BASE_URL}/board/${cardId}/card`
+				`${import.meta.env.VITE_BASE_URL}/board/${boardId}/card`
 			);
+
 			const dataCard = await responseCard.json();
 
-			console.log(dataCard);
-			setBoardData(dataCard);
-		};
-	});
+			setCardData(dataCard);
+		})();
+	}, [boardId]);
+	return (
+		<div className="container">
+			<header className="header-container">
+				<h1 style={{ color: colors.primary }}>👏Kudos Board👏</h1>
+			</header>
+			<div className="create-theme-container">
+				{/* <button
+                    className="create-btn"
+                    style={{
+                        background: colors.button,
+                        color: colors.background,
+                    }}
+                    onClick={() => {
+                        setToggleCreateModal(!toggleCreateModal);
+                    }}
+                >
+                    Create a New Board
+                </button> */}
+				<ChangeTheme />
+			</div>
+			<h3>{boardData.title}</h3>
+		</div>
+	);
 }
