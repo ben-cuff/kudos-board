de396fc7dd6ffcac94027d8cef54165190177c65
Ben Cuff <bencuff14@gmail.com>
2025-06-12 12:09:10 -0700

    refactor CreateCardModal to use CreateCardForm component and separate styles

---
+++ front-end-kudos-board/src/components/card/create-card-form.jsx
@@ -0,0 +1,76 @@
+import { Gif, Grid } from "@giphy/react-components";
+import PropTypes from "prop-types";
+import useCreateCard from "../../hooks/use-create-card";
+import "./create-card-form.css";
+
+export default function CreateCardForm({
+	handleSubmitCreateModal,
+	setSelectedGif,
+}) {
+	const { searchTerm, setSearchTerm, gifData, setGifData, fetchGifs } =
+		useCreateCard();
+	return (
+		<form onSubmit={handleSubmitCreateModal}>
+			<input
+				type="text"
+				id="card-message"
+				name="message"
+				required
+				placeholder="message"
+			/>
+			<input
+				type="text"
+				id="card-author"
+				name="author"
+				placeholder="author (optional)"
+			/>
+			<input
+				type="text"
+				id="card-gif"
+				name="gif"
+				required
+				onChange={(e) => setSearchTerm(e.target.value)}
+				placeholder="gif from giphy"
+			/>
+			{searchTerm && !gifData && (
+				<Grid
+					width={300}
+					columns={4}
+					onGifClick={(gif) => {
+						setSelectedGif(gif.id);
+						setGifData(gif);
+					}}
+					fetchGifs={fetchGifs}
+					key={searchTerm}
+					noLink={true}
+				/>
+			)}
+			{gifData && (
+				<div className="selected-gif-container">
+					<Gif gif={gifData} width={200} noLink={true} />
+					<button
+						type="button"
+						className="reset-gif-btn"
+						onClick={() => {
+							setGifData(null);
+							setSelectedGif("");
+						}}
+					>
+						Reset GIF
+					</button>
+				</div>
+			)}
+			<button type="submit">Create Board</button>
+		</form>
+	);
+}
+
+CreateCardForm.propTypes = {
+	handleSubmitCreateModal: PropTypes.func.isRequired,
+	setSearchTerm: PropTypes.func.isRequired,
+	searchTerm: PropTypes.string,
+	gifData: PropTypes.object,
+	setSelectedGif: PropTypes.func.isRequired,
+	setGifData: PropTypes.func.isRequired,
+	fetchGifs: PropTypes.func.isRequired,
+};
--- front-end-kudos-board/src/components/card/create-card-modal.jsx
+++ front-end-kudos-board/src/components/card/create-card-modal.jsx
@@ -1,7 +1,6 @@
-import { Gif, Grid } from "@giphy/react-components";
 import propTypes from "prop-types";
-import useCreateCard from "../../hooks/use-create-card";
 import { useTheme } from "../../hooks/use-theme";
+import CreateCardForm from "./create-card-form";
 import "./create-card-modal.css";
 
 export default function CreateCardModal({
@@ -10,8 +9,6 @@ export default function CreateCardModal({
 	setSelectedGif,
 }) {
 	const { colors } = useTheme();
-	const { searchTerm, setSearchTerm, gifData, setGifData, fetchGifs } =
-		useCreateCard();
 
 	return (
 		<div className="overlay-style">
@@ -20,58 +17,10 @@ export default function CreateCardModal({
 				style={{ background: colors.background }}
 			>
 				<h2>Create New Card</h2>
-				<form onSubmit={handleSubmitCreateModal}>
-					<input
-						type="text"
-						id="card-message"
-						name="message"
-						required
-						placeholder="message"
-					/>
-					<input
-						type="text"
-						id="card-author"
-						name="author"
-						placeholder="author (optional)"
-					/>
-					<input
-						type="text"
-						id="card-gif"
-						name="gif"
-						required
-						onChange={(e) => setSearchTerm(e.target.value)}
-						placeholder="gif from giphy"
-					/>
-					{searchTerm && !gifData && (
-						<Grid
-							width={300}
-							columns={4}
-							onGifClick={(gif) => {
-								setSelectedGif(gif.id);
-								setGifData(gif);
-							}}
-							fetchGifs={fetchGifs}
-							key={searchTerm}
-							noLink={true}
-						/>
-					)}
-					{gifData && (
-						<div className="selected-gif-container">
-							<Gif gif={gifData} width={200} noLink={true} />
-							<button
-								type="button"
-								className="reset-gif-btn"
-								onClick={() => {
-									setGifData(null);
-									setSelectedGif("");
-								}}
-							>
-								Reset GIF
-							</button>
-						</div>
-					)}
-					<button type="submit">Create Board</button>
-				</form>
+				<CreateCardForm
+					handleSubmitCreateModal={handleSubmitCreateModal}
+					setSelectedGif={setSelectedGif}
+				/>
 				<button
 					className="close-btn"
 					onClick={() => {
