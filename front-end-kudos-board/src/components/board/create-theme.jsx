51ef5abf9ffa325294e7cf27e6f33086d69556d9
Ben Cuff <bencuff14@gmail.com>
2025-06-12 11:09:22 -0700

    add CreateTheme component with styling for board creation

---
+++ front-end-kudos-board/src/components/board/create-theme.jsx
@@ -0,0 +1,34 @@
+import propTypes from "prop-types";
+import { useTheme } from "../../hooks/use-theme";
+import ChangeTheme from "../change-theme";
+import "./create-theme.css";
+
+export default function CreateTheme({
+	setToggleCreateModal,
+	toggleCreateModal,
+}) {
+	const { colors } = useTheme();
+
+	return (
+		<div className="create-theme-container">
+			<button
+				className="create-btn"
+				style={{
+					background: colors.button,
+					color: colors.background,
+				}}
+				onClick={() => {
+					setToggleCreateModal(!toggleCreateModal);
+				}}
+			>
+				Create a New Board
+			</button>
+			<ChangeTheme />
+		</div>
+	);
+}
+
+CreateTheme.propTypes = {
+	setToggleCreateModal: propTypes.func.isRequired,
+	toggleCreateModal: propTypes.bool.isRequired,
+};
